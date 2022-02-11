d35b766c800772011637589370d9f226bbe3e976
dom <>
2022-02-11 02:21:07 +0000

    Add number of results setting.

--- brotab_ulauncher/extension.py
+++ brotab_ulauncher/extension.py
@@ -19,8 +19,6 @@ from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from brotab_ulauncher.client import BrotabClient
 from brotab_ulauncher.listeners import KeywordQueryEventListener, ItemEnterEventListener
 
-DISPLAY_MAX_RESULTS = 10
-
 
 class BrotabExtension(Extension):
     """ Main Extension Class  """
@@ -64,7 +62,10 @@ class BrotabExtension(Extension):
         items = []
         tabs = self.brotab_client.search_tabs(event.get_argument())
 
-        for tab in tabs[:DISPLAY_MAX_RESULTS]:
+        max_results = int(extension.preferences["max_results"])
+        self.logger.info("maxres:%s" % max_results)
+
+        for tab in tabs[:max_results]:
             data = {"tab": tab["prefix"], "mode": self.mode}
 
             if extension.preferences["show_url"] == "Yes":
